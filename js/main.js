d885b0357ab8f2cc5da069db690e425582abfb5c
DJ <DJrumph@gmail.com>
2018-09-19 02:26:30 -0400

    adding speaking

--- js/main.js
+++ js/main.js
@@ -17,10 +17,43 @@ let voices = [];
 
 const getVoices = () => {
     voices = synth.getVoices();
-    console.log(voices);
+   
+    //loop through voices and greate an option for each one
+    voices.forEach(voice => {
+        //create option element
+        const option = document.createElement('option');
+        option.textContent = voice.name + '(' + voice.lang + ')';
+        //Set needed opton attributes
+        option.setAttribute('data-lang', voice.lang);
+        option.setAttribute('data-name', voice.name);
+        voiceSelect.appendChild(option);
+    });
 };
 
 getVoices();
 if(synth.onvoiceschanged !== undefined){
     synth.onvoiceschanged = getVoices;
+}
+
+//Speak
+
+const speak = () => {
+    //check is speaking
+    if(synth.speaking){
+        console.error("already speaking");
+        return;
+    }
+
+    if(textInput.value !== ''){
+        //get speak text
+        const speakText = new SpeechSynthesisUtterance(textInput.value);
+    }
+    //speak end
+    speakText.onend = e  => {
+        console.log("Done speaking");
+    }
+    //speak error
+    speakText.onerrer = e =>{
+        console.log('Error');
+    }
 }
